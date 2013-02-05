206c0e0b1abdc5f2a0be6bd43ded577bc4aa2c42
Kalail <kalail@ubuntu.(none)>
2013-02-05 04:18:45 -0600

    Add fabfile commands

--- piggybank/fabfile.py
+++ piggybank/fabfile.py
@@ -3,6 +3,12 @@ from fabric.api import *
 env.hosts = ['96.126.122.107']
 env.user = 'kalail'
 
-def upload_local_settings():
+def upload_settings():
 	put('piggybank/settings/local.py', '~/envs/piggybank/piggybank/piggybank/piggybank/settings', use_sudo=False)
-	# code_dir = '/srv/django/myproject'
+
+def deploy():
+	# Pull from github
+	with cd('envs/piggybank/piggybank'):
+		run('git pull')
+	# Copy over local settings
+	put('piggybank/settings/local.py', '~/envs/piggybank/piggybank/piggybank/piggybank/settings', use_sudo=False)
